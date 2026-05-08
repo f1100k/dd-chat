528136a24147a47763548ceefc145e5922e72213
f1100k <fpestelato@gmail.com>
2026-05-08 13:04:32 -0300

    build: improve general configuration adding pnpm catalog, format on save and adjust lint configs

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,18 @@
-export {}
+import { PrismaClient } from "@prisma/client"
+
+declare global {
+	var __prisma: PrismaClient | undefined
+}
+
+export const prisma =
+	globalThis.__prisma ??
+	new PrismaClient({
+		log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+	})
+
+if (process.env.NODE_ENV !== "production") {
+	globalThis.__prisma = prisma
+}
+
+export type { Prisma } from "@prisma/client"
+export * from "@prisma/client"
